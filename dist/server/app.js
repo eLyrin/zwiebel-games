ef73b716c53113e8ff7d8df9bd3b733765dfddc3
Christopher Knips <c.knips@gmail.com>
2022-09-16 17:01:05 +0200

    ....

--- dist/server/app.js
+++ dist/server/app.js
@@ -1,4 +1,4 @@
-import * as express from "express";
+import express from "express";
 const app = express();
 app.use(express.static('client'));
 app.listen(process.env["PORT"] || 3001, () => console.log("foo on port: " + process.env["PORT"]));
---
+++ server/src/app.ts
@@ -0,0 +1,5 @@
+import express from "express";
+
+const app = express();
+app.use(express.static('client'));
+app.listen(process.env["PORT"] || 3001, () => console.log("foo on port: " + process.env["PORT"]));
