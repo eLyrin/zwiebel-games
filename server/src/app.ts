93423dafaab33dd73842c09f8132d6be38ad1cee
Christopher Knips <c.knips@gmail.com>
2022-09-17 15:02:32 +0200

    .....

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,20 @@
 import express from "express";
+import { createServer } from "http";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-app.use(express.static('client'));
-app.listen(process.env["PORT"] || 3001, () => console.log("foo on port: " + process.env["PORT"]));
+const httpServer = createServer(app);
+
+
+app.use(express.static(path.join(__dirname, "/client")));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/client/index.html")));
+
+// app.listen(process.env["PORT"] || 3001, () => console.log("foo on port: " + process.env["PORT"]));
+httpServer.listen(process.env["PORT"] || 3001);
+console.log("hallo weldsdst");
